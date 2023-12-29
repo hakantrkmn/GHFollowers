1aaacbf25341234c718a0c24c6cb479573b61227
Hakan Türkmen <hakannturkmen@gmail.com>
2023-12-29 18:14:52 +0300

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Hakan Türkmen on 29.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
